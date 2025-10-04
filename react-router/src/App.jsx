c3bda09c83d49bc2f067803fd125b01497e81f74
M ALI RAZA <casim9530@gmail.com>
2025-10-04 15:01:25 -0700

    Clickme

---
+++ react-router/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+import react from "react";
+import Header from "./component/Header/Header";
+import Footer from "./component/Footer/Footer";
+import Home from "./component/Home/Home";
+
+function App() {
+  return <></>;
+}
+
+export default App;
